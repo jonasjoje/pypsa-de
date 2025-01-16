a2e3dd2ab557626bd47dc878a54e0e5c1c0d4d16
jonasjoje <jonasjoje@web.de>
2025-01-16 11:14:12 +0100

    Add minimal download skript.

--- scripts/retrieve_DEA_el_and_dh_data.py
+++ scripts/retrieve_DEA_el_and_dh_data.py
@@ -2,3 +2,24 @@
 # retrieve from data set from danish energy agency
 # https://ens.dk/en/analyses-and-statistics/technology-data-generation-electricity-and-district-heating
 
+# todo: als snakemake skript
+# todo: überprüfen ob (aktuellste) datei vorhanden ist
+# todo: vergleichen mit anderen retrieve skripts
+
+import urllib.request
+import os
+
+# URL of the latest Excel file
+download_url = "https://ens.dk/media/5795/download"
+
+# Ensure the script's directory is used as the base path
+script_dir = os.path.dirname(os.path.abspath(__file__))
+path_out = os.path.join(script_dir, "..", "data", "DEA_electricity_district_heat_data_sheet.xlsx")
+
+# Ensure the output directory exists
+#os.makedirs(os.path.dirname(path_out), exist_ok=True)
+
+# Download and save the file
+urllib.request.urlretrieve(download_url, path_out)
+
+print('Download complete.')
